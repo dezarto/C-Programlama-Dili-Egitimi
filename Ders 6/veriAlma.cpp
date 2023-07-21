269b1eaafb7747f40861a7d0d1291dadb36d2234
dezarto <74088316+dezarto@users.noreply.github.com>
2023-07-21 17:56:29 +0300

    Add files via upload

---
+++ Ders 6/veriAlma.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+/*
+	printf() = ekrana bastırma
+	scanf() = kullanıcıdan veri alma
+*/
+
+int main() {
+
+	/*
+	int yas;
+
+	printf("Yas giriniz: ");
+	scanf("%d", &yas);
+
+	printf("Girilen yas: %d", yas);
+	*/
+
+	float kilo;
+
+	printf("Lutfen alacaginiz kiloyu giriniz: ");
+	scanf("%f", &kilo);
+
+	printf("Girilen kilo: %.2f", kilo);
+
+	return 0;
+}
