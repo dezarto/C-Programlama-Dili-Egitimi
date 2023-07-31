b1964e1759fb4ba9e4410f0d5f3f05c6c34642bf
dezarto <74088316+dezarto@users.noreply.github.com>
2023-07-31 17:11:06 +0300

    Add files via upload

---
+++ Ders 10/localVeGlobalDegiskenler.c
@@ -0,0 +1,39 @@
+﻿#include<stdio.h>
+
+/*
+*	Local ve Global Değişken Tanımlama
+* 
+*	
+*	Local: Bulunduğu alanda geçerli.
+*	
+*	Global: Her yerde geçerli.
+* 
+*/
+
+int a = 10; //global
+
+int main() {
+
+	//int a = 10; //fonksiyon içi global - fonksiyonlar arası local
+	
+	printf("a: %d\n", a);
+
+	if (a > 1) {
+		int b = 5; // local
+		printf("b: %d\n", b);
+		printf("a: %d\n", a);
+	}
+
+	//printf("b: %d\n", b);
+
+	test();
+
+	return 0;
+}
+
+int test() {
+
+	printf("test a: %d\n", a);
+
+	return 0;
+}
