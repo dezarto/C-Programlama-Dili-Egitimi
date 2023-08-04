991fcec7a8b60faa4f73c7d86faf15c19d9dcbd0
dezarto <74088316+dezarto@users.noreply.github.com>
2023-08-04 15:16:46 +0300

    Add files via upload

---
+++ Ders 13/booleans.c
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include<stdbool.h>
+
+/*
+*		Booleans
+*
+*		true = 1
+*		false = 0
+*
+*		Kütüphane
+*
+*		#include <stdbool.h>
+*
+*		Örnek Kullanım
+*
+*		bool değişkenİsmi = true;
+*		bool değişkenİsmi = false;
+*/
+
+int main() {
+	bool istenen = false;
+	int a = 5;
+
+	/*bool dogruluk = true;
+	bool yanlis = false;*/
+
+	/*printf("True: %d\n", dogruluk);
+	printf("False: %d", yanlis);*/
+
+	printf("Baslangic: %d\n", istenen);
+	if (a < 10) {
+		istenen = true;
+		printf("Bitis: %d\n", istenen);
+	}
+
+	return 0;
+}
