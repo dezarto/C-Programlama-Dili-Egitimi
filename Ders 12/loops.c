3c83f89465e5250fedceee3db23172dff37b0184
dezarto <74088316+dezarto@users.noreply.github.com>
2023-08-04 15:15:03 +0300

    Add files via upload

---
+++ Ders 12/loops.c
@@ -0,0 +1,71 @@
+﻿#include<stdio.h>
+
+/*
+ 
+ Döngüler | for() | while() | do{}while() |
+
+for (ilk-değer-atama; koşul; artırma/azaltma)
+{
+    //
+    // kodlar
+    //
+}
+
+*************************************
+
+while (ifade)
+{
+    // kodlar
+}
+
+*************************************
+
+do
+{
+    // kodlar
+    
+} while (ifade);
+ 
+*/
+
+
+int main() {
+
+    // FOR DÖNGÜSÜ YOL 1
+
+    /*for (int a = 0; a < 10; a++) {
+        printf("Merhaba dunya!\n");
+    }*/
+
+    // FOR DÖNGÜSÜ YOL 2
+    
+    /*int i = 0;
+    for (;;)
+    {
+        if (i < 10) {
+            printf("Merhaba dunya!\n");
+            i++;
+        }
+        else {
+            break;
+        }
+    }*/
+
+    // WHİLE DÖNGÜSÜ
+
+    /*int a = 0;
+    while (a < 10) {
+        printf("Merhaba dunya!\n");
+        a++;
+    }*/
+
+    // DO WHİLE DÖNGÜSÜ
+
+    /*int a = 0;
+    do {
+        printf("Merhaba dunya!\n");
+        a++;
+    } while (a < 10);*/
+
+	return 0;
+}
