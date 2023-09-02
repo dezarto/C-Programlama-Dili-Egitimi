dc3641f901b93fd073d24325fae279dd267e3c32
dezarto <74088316+dezarto@users.noreply.github.com>
2023-09-02 17:34:44 +0300

    Add files via upload

---
+++ Ders 3 - Ekrana yazı yazdırma işlevi. printf()/MerhabaDunya.c
@@ -0,0 +1,17 @@
+﻿/*  Yazilim Karargahi
+	
+	Semir Kimyonsen
+	
+	printf() = Ekrana çıktı bastırılmasını sağlar
+	\n = Satır atlamamızı sağlar
+*/
+#include<stdio.h>
+
+int main() {
+
+	printf("Merhaba dunya!\n");
+	printf("\n");
+	printf("Semir Kimyonsen");
+
+	return 0;
+}
