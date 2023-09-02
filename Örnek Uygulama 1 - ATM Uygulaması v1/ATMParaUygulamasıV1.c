6cbddd563d3e58efbae349dbea4db1f46baf6b47
dezarto <74088316+dezarto@users.noreply.github.com>
2023-09-02 18:30:04 +0300

    Add files via upload

---
+++ Örnek Uygulama 1 - ATM Uygulaması v1/ATMParaUygulamasıV1.c
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+
+	int islemTuru;
+	float anaPara = 250, tutarBilgisi, istenilenParaMiktari;
+
+	printf("Yapmak istenilen islem nedir ?\n");
+	printf(" Para cekme - 1\n Para Yatirma - 2\n Bakiye Bilgisi - 3\n");
+	printf("Lutfen yapmak istediginiz islemin numarasini giriniz: ");
+	scanf("%d", &islemTuru);
+
+	if (islemTuru == 1) {
+		printf("Nekadar para cekmek istersiniz: ");
+		scanf("%f", &istenilenParaMiktari);
+
+		if (anaPara >= istenilenParaMiktari) {
+			anaPara = anaPara - istenilenParaMiktari;
+			printf("Paraniz basarli bir sekilde cekilmistir. \nGuncel bakiyeniz: %f", anaPara);
+		}
+		else {
+			printf("Yetersiz bakiye!");
+		}
+	}
+	else if (islemTuru == 2) {
+		printf("Nekadar para yatırmak istersiniz: ");
+		scanf("%f", &istenilenParaMiktari);
+
+		if (istenilenParaMiktari < 0) {
+			printf("Girilen tutar gecersizdir.");
+		}
+		else if (istenilenParaMiktari == 0) {
+			printf("Girilen tutar sifirdir islem iptal edildi.");
+		}
+		else {
+			anaPara = anaPara + istenilenParaMiktari;
+			printf("Bakiye basarili bir sekilde yatirilmistir.\nGuncel bakiyeniz: %f", anaPara);
+		}
+	}
+	else if (islemTuru == 3) {
+		printf("Bakiye miktari: %f", anaPara);
+	}
+	else {
+		printf("\nGirilen deger gecersizdir, Lutfen kontrol ediniz!\n");
+	}
+
+	return 0;
+}
