980837fd9c925657c56e862c7944d2a43922932f
dezarto <74088316+dezarto@users.noreply.github.com>
2024-03-30 20:41:47 +0300

    Add files via upload

---
+++ Ders 17 - Pointer Nedir (İşaretçiler)/call by value - call by reference.c
@@ -0,0 +1,22 @@
+﻿// Call by Reference
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+	int tampon;
+
+	tampon = *x;
+	*x = *y;
+	*y = tampon;
+
+	printf("Fonksiyon ici\na: %d\n b: %d\n", *x, *y);
+}
+
+int main()
+{
+	int a = 5, b = 10;
+
+	swap(&a, &b);
+	printf("a: %d\n b: %d", a, b);
+
+	return 0;
+}
