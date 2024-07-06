eac085495215549912fb43071fa81413d6979698
dezarto <74088316+dezarto@users.noreply.github.com>
2024-07-07 00:39:39 +0300

    Add files via upload

---
+++ Ders 18 - Strings/strings.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+
+/*
+*	String Fonksiyonları
+* 
+*	strlen(par1); // par1 in uzunluğunu verir
+*	strcpy(par1, par2); // par2 yi par1 e kopyalar
+*	strcat(par1, par2); // par2 yi par1 e sağdan ekler
+*/
+
+int main() {
+
+	char test1[14] = "Merhaba";
+	char test2[] = " Dunya";
+
+	strcat(test1, test2);
+
+	printf("Test1: %s\n", test1);
+	printf("Test2: %s\n", test2);
+
+	/*strcpy(test1, test2);
+
+	printf("Test1: %s\n", test1);
+	printf("Test2: %s", test2);*/
+
+	/*char test[] = "merhaba";
+
+	printf("Karakter: %s\n", test);
+	printf("Karakter miktari 1: %d\n", strlen(test));
+	printf("Karakter miktari 2: %d\n", sizeof(test));*/
+
+	return 0;
+}
