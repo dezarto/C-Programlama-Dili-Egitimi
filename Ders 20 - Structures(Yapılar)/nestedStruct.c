c583f19fa5b17373ff400cda28fc4dd325cf7ef3
dezarto <74088316+dezarto@users.noreply.github.com>
2024-08-01 18:17:37 +0300

    Add files via upload

---
+++ Ders 20 - Structures(Yapılar)/nestedStruct.c
@@ -0,0 +1,102 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+// Yöntem 1
+
+//struct adres {
+//	char sehir[15];
+//	char ilce[15];
+//	int postaKodu;
+//};
+//
+//struct araba {
+//	char marka[10];
+//	char model[10];
+//	int yil;
+//	char vites;
+//};
+//
+//struct arabaKirala {
+//	struct adres a1;
+//	struct araba c1;
+//}araba2, araba3 = { "Istanbul","Zeytinburnu", 82000, "Toyota", "Hilux", 2020, 'm' };
+
+// Yöntem 2
+
+struct adres {
+	char sehir[15];
+	char ilce[15];
+	int postaKodu;
+};
+
+struct arabaKirala {
+	struct adres a1;
+	struct araba {
+		char marka[10];
+		char model[10];
+		int yil;
+		char vites;
+	}c1;
+}araba2, araba3 = { "Istanbul","Zeytinburnu", 82000, "Toyota", "Hilux", 2020, 'm' };
+
+int main() {
+	// Yöntem 1
+	struct arabaKirala araba1;
+
+	strcpy(araba1.a1.sehir, "Istanbul");
+	strcpy(araba1.a1.ilce, "Bahcelievler");
+	araba1.a1.postaKodu = 81000;
+	
+	strcpy(araba1.c1.marka, "Toyota");
+	strcpy(araba1.c1.model, "Corolla");
+	//strcpy(araba1.c1.vites, "o"); YANLIŞ
+	araba1.c1.vites = 'o';
+	araba1.c1.yil = 2022;
+
+	printf("--- Araba Kiralama ---\n");
+	printf("Araba Konumu 1: \n");
+	printf("Sehir: %s\n", araba1.a1.sehir);
+	printf("Ilce: %s\n", araba1.a1.ilce);
+	printf("Posta kodu: %d\n", araba1.a1.postaKodu);
+	printf("Marka: %s\n", araba1.c1.marka);
+	printf("Model: %s\n", araba1.c1.model);
+	printf("Vites: %c\n", araba1.c1.vites);
+	printf("Yil: %d\n", araba1.c1.yil);
+	printf("-------------------\n");
+	
+	// Yöntem 2
+
+	strcpy(araba2.a1.sehir, "Istanbul");
+	strcpy(araba2.a1.ilce, "Bahcelievler");
+	araba2.a1.postaKodu = 81000;
+
+	strcpy(araba2.c1.marka, "Toyota");
+	strcpy(araba2.c1.model, "Corolla");
+	araba2.c1.vites = 'o';
+	araba2.c1.yil = 2022;
+
+	printf("Araba Konumu 2: \n");
+	printf("Sehir: %s\n", araba2.a1.sehir);
+	printf("Ilce: %s\n", araba2.a1.ilce);
+	printf("Posta kodu: %d\n", araba2.a1.postaKodu);
+	printf("Marka: %s\n", araba2.c1.marka);
+	printf("Model: %s\n", araba2.c1.model);
+	printf("Vites: %c\n", araba2.c1.vites);
+	printf("Yil: %d\n", araba2.c1.yil);
+	printf("-------------------\n");
+
+	// Yöntem 3
+
+	printf("Araba Konumu 3: \n");
+	printf("Sehir: %s\n", araba3.a1.sehir);
+	printf("Ilce: %s\n", araba3.a1.ilce);
+	printf("Posta kodu: %d\n", araba3.a1.postaKodu);
+	printf("Marka: %s\n", araba3.c1.marka);
+	printf("Model: %s\n", araba3.c1.model);
+	printf("Vites: %c\n", araba3.c1.vites);
+	printf("Yil: %d\n", araba3.c1.yil);
+	printf("-------------------\n");
+
+	return 0;
+}
---
+++ Ders 20 - Structures(Yapılar)/structures.c
@@ -0,0 +1,70 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+struct bilgiler {
+	int yariyil;
+	float gno;
+	char isim[15];
+	char bolum[20];
+}ogrenci3 = { 3, 2.50, "Hamza", "Bilgisayar Muh." }, ogrenci4;
+
+int main() {
+	// Yöntem 1
+	struct bilgiler ogrenci1;
+
+	ogrenci1.yariyil = 6;
+	ogrenci1.gno = 3.10;
+	//ogrenci1.isim = "Adem"; YANLIŞ
+	strcpy(ogrenci1.isim, "Adem");
+	//ogrenci1.bolum = "Bilgisayar Muh."; YANLIŞ
+	strcpy(ogrenci1.bolum, "Bilgisayar Muh.");
+
+	printf("Yariyil: %d\n", ogrenci1.yariyil);
+	printf("GNO: %.2f\n", ogrenci1.gno);
+	printf("Isim: %s\n", ogrenci1.isim);
+	printf("Bolum: %s\n", ogrenci1.bolum);
+	printf("----------------\n");
+	// Yöntem 2
+
+	struct bilgiler ogrenci2 = { 5, 2.80, "Enes", "Bilgisayar Muh." };
+
+	printf("Yariyil: %d\n", ogrenci2.yariyil);
+	printf("GNO: %.2f\n", ogrenci2.gno);
+	printf("Isim: %s\n", ogrenci2.isim);
+	printf("Bolum: %s\n", ogrenci2.bolum);
+	printf("----------------\n");
+
+	// Yöntem 3
+	
+	printf("Yariyil: %d\n", ogrenci3.yariyil);
+	printf("GNO: %.2f\n", ogrenci3.gno);
+	printf("Isim: %s\n", ogrenci3.isim);
+	printf("Bolum: %s\n", ogrenci3.bolum);
+	printf("----------------\n");
+
+	// Yöntem 4
+
+	ogrenci4.yariyil = 6;
+	ogrenci4.gno = 3;
+	strcpy(ogrenci4.isim, "Onur");
+	strcpy(ogrenci4.bolum, "Bilgisayar Muh.");
+
+	printf("Yariyil: %d\n", ogrenci4.yariyil);
+	printf("GNO: %.2f\n", ogrenci4.gno);
+	printf("Isim: %s\n", ogrenci4.isim);
+	printf("Bolum: %s\n", ogrenci4.bolum);
+	printf("----------------\n");
+
+	// Verileri Güncelleme
+
+	ogrenci1.yariyil = 7;
+	ogrenci1.gno = 3.15;
+
+	printf("Yariyil: %d\n", ogrenci1.yariyil);
+	printf("GNO: %.2f\n", ogrenci1.gno);
+	printf("Isim: %s\n", ogrenci1.isim);
+	printf("Bolum: %s\n", ogrenci1.bolum);
+
+	return 0;
+}
