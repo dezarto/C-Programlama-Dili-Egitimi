7813790500b6dba00479771d901709f2b5ebf98d
dezarto <74088316+dezarto@users.noreply.github.com>
2024-09-13 07:34:16 +0300

    Add files via upload

---
+++ Ders 21 - Unions/unions.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+union Veri {
+	int i;
+	float f;
+	char str[20];
+}uVeri;
+
+typedef struct {
+	int i;
+	float f;
+	char str[20];
+}sVeri;
+
+int main() {
+
+	printf("Boyut union: %d\n", sizeof(uVeri));
+	printf("Boyut struct: %d\n", sizeof(sVeri));
+
+	uVeri.i = 10;
+	uVeri.f = 3.25;
+	//uVeri.str = "Enes"; YANLIŞ
+	//strcpy(uVeri.str, "Enes");
+
+	printf("int i: %d\n", uVeri.i);
+	printf("float f: %f\n", uVeri.f);
+	printf("string str: %s\n", uVeri.str);
+
+	return 0;
+}
