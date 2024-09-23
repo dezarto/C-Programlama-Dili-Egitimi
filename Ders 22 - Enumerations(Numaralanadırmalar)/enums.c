7ee134412e186e7d163ac10c58195bdb27aa7ac6
dezarto <74088316+dezarto@users.noreply.github.com>
2024-09-23 14:30:40 +0300

    Add files via upload

---
+++ Ders 22 - Enumerations(Numaralanadırmalar)/enums.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+enum Level {
+	KUCUK = 1,
+	ORTA,
+	BUYUK,
+	COK_BUYUK
+};
+
+int main() {
+	// Yöntem 1
+	enum Level l1;
+	l1 = ORTA;
+
+	// Yöntem 2
+	
+	//enum Level l1 = KUCUK;
+
+	printf("Level: %d\n", l1);
+
+	switch (l1)
+	{
+	case KUCUK: printf("Kucuk boy icecek aldiniz.\n"); break;
+	case ORTA: printf("Orta boy icecek aldiniz.\n"); break;
+	case BUYUK: printf("Buyuk boy icecek aldiniz.\n"); break;
+	case COK_BUYUK: printf("Cok Buyuk boy icecek aldiniz.\n"); break;
+	default: printf("Gecersiz...\n");
+		break;
+	}
+
+
+	return 0;
+}
